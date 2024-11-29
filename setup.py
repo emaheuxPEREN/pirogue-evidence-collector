68014586c5a9e05e2e99083011be8197c6a79817
U039b <git@0x39b.fr>
2024-11-29 08:08:40 +0100

    Bump to v1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pirogue-evidence-collector",
-    version="1.0.2",
+    version="1.0.3",
     author="U+039b",
     author_email="hello@pts-project.org",
     description="A set of tools to collect evidence from mobile devices",
